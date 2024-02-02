cbda53127648f333e0f80414b922302dfe8d2f08
Igor Carvalho <iggor1935@hotmail.com>
2024-02-02 20:29:08 -0300

    Add project files.

---
+++ Expert.Gov.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Expert.Gov.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
