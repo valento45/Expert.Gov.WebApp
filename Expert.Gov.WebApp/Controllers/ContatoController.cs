7b1bb9ec3a2f7ba23b83a8223d74554d4cca4af0
Igor Carvalho <iggor1935@hotmail.com>
2024-02-03 21:53:03 -0300

    Updated

---
+++ Expert.Gov.WebApp/Controllers/ContatoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Expert.Gov.WebApp.Controllers
+{
+    public class ContatoController : Controller
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> SolicitarMelhorias()
+        {
+            return View();
+        }
+    }
+}
