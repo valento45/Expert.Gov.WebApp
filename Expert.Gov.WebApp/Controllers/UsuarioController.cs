99576bd4a264ddf1031ed81916e3483ea047bf30
develooper <claytonfribeiro@outlook.com>
2024-02-06 00:02:58 -0300

    Commit

---
+++ Expert.Gov.WebApp/Controllers/UsuarioController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Expert.Gov.WebApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
